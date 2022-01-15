9a1225c3e76d6f0855a83ccb2cb1c17fa12b6304
Soufiane Lemrabet <soufiane.lemrabet@grenoble-inp.org>
2022-01-15 13:46:14 +0000

    Update DeclVar.java

--- src/main/java/fr/ensimag/deca/tree/DeclVar.java
+++ src/main/java/fr/ensimag/deca/tree/DeclVar.java
@@ -84,7 +84,9 @@ public class DeclVar extends AbstractDeclVar {
     
     @Override
     public void decompile(IndentPrintStream s) {
-        throw new UnsupportedOperationException("not yet implemented");
+        type.decompile(s);
+        varName.decompile(s);
+        initialization.decompile(s);
     }
 
     @Override
