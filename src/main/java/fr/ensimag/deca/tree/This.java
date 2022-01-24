c7d663e0b756e91d58567a3249d995acb5fdf44e
Kloua <anas.klou@grenoble-inp.org>
2022-01-24 12:27:20 +0100

    rectifier

--- src/main/java/fr/ensimag/deca/tree/This.java
+++ src/main/java/fr/ensimag/deca/tree/This.java
@@ -8,10 +8,12 @@ import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.deca.tools.SymbolTable;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.RegisterOffset;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.STORE;
 
 import java.io.PrintStream;
-
-
 
 /**
  *
@@ -20,34 +22,39 @@ import java.io.PrintStream;
  */
 public class This extends AbstractExpr {
     @Override
-    public  Type verifyExpr(DecacCompiler compiler,
+    public Type verifyExpr(DecacCompiler compiler,
             EnvironmentExp localEnv, ClassDefinition currentClass)
-            throws ContextualError{
-            SymbolTable.Symbol classe0 = SymbolTable.creerSymbol("0");
-            if(currentClass == null){
-                throw new ContextualError(" This est défini que dans des classes",getLocation());
-            }
-            if( currentClass.getType().getName().equals(classe0)){
-                throw new ContextualError(" This est défini que dans des classes",getLocation());
-            }
-            ClassType type = new ClassType(currentClass.getType().getName(),getLocation(), currentClass.getSuperClass());
-            setType(type);
-            return type;
-            }
+            throws ContextualError {
+        SymbolTable.Symbol classe0 = SymbolTable.creerSymbol("0");
+        if (currentClass == null) {
+            throw new ContextualError(" This est défini que dans des classes", getLocation());
+        }
+        if (currentClass.getType().getName().equals(classe0)) {
+            throw new ContextualError(" This est défini que dans des classes", getLocation());
+        }
+        ClassType type = new ClassType(currentClass.getType().getName(), getLocation(), currentClass.getSuperClass());
+        setType(type);
+        return type;
+    }
+
     @Override
     protected void iterChildren(TreeFunction f) {
-        //throw new UnsupportedOperationException("Not yet supported");
+        // throw new UnsupportedOperationException("Not yet supported");
     }
+
     @Override
     protected void prettyPrintChildren(PrintStream s, String prefix) {
-        //throw new UnsupportedOperationException("Not yet supported");
+        // throw new UnsupportedOperationException("Not yet supported");
     }
+
     @Override
     public void decompile(IndentPrintStream s) {
         s.print("this");
     }
+
     @Override
-    public  void codeGenExpr(DecacCompiler compiler,int n){
+    public void codeGenExpr(DecacCompiler compiler, int n) {
         //
+        compiler.addInstruction(new LOAD(new RegisterOffset(-2, Register.LB), Register.getR(3)));
     }
 }
